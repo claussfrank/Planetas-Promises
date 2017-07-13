5f36bdf459d0c27fdbdf09543a26405d588a1a35
Clauss Franco <you@example.com>
2017-07-13 18:32:59 -0500

    Primera funcionabilidad Corriendo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,58 @@
 var ajax = new XMLHttpRequest();
-var url ="data/earth-like-results.json"
+var url = "data/earth-like-results.json";
 
-//funcion  que  nos  ayuda a jalar  la  apis
 
-function getJSON(url){
-	return new Promise(function(resolve,reject){
+//funcion  que  nos  ayuda a jalar el Json
+
+function getJSON(url) {
+	return new Promise (function (resolve,reject) {
+		//peticion ajax
 		var ajax = new XMLHttpRequest();
-		ajax.open("GET",url);
+		ajax.open("GET", url);
 		ajax.send();
-		ajax.onreadystatechange = function(){
+		ajax.onreadystatechange = function() {
 			if(ajax.readyState == 4){
+				// te  devuelve  un string  y  para convertirlo en  un  objetp le pones JSON.parse
 				resolve(JSON.parse(ajax.responseText));
-				
+
 			}
 		}
 	}) 
 }
 
 getJSON(url)
-.then (function(mensaje){
-	console.log(mensaje)
-	return(getJSON(mensaje.results[0]))
+	.then (function(mensaje) {
+	return Promise.all(mensaje.results.map(getJSON));
 })
-.then(function(resultado){
-	console.log(resultado)
-})
+	.then (function(resultado){
+	  resultado.forEach(function(planeta){
+		  
+		var nombrePlaneta = planeta.pl_name;
+		var imagenPlaneta = planeta.pl_name;  
+		var fecha = planeta.pl_disc;
+		var distancia = planeta.st_dist;
+		var cards = document.getElementById("Cards"); 
+		var nuevaPlantilla = "";  
+		
+
+		var plantilla = 
+			'<div class="col s12 m6">'+
+			'<div class="card">'+
+			'<div class="card-image">'+
+			'<img src="static/img/**imagen**.jpg">'+
+			'<span class="card-title">**planet**</span>'+
+			'</div>'+
+			'<div class="card-content">'+
+			'<p><strong>Discover in:</strong><span>**fecha**</span></p>'+
+			'<p><strong>Distance:</strong><span>**distancia**</span></p>'+
+			'</div>'+
+			'</div>'+
+			'</div>';
+	
+
+		nuevaPlantilla += plantilla.replace('**planet**',nombrePlaneta).replace('**fecha**',fecha).replace('**distancia**',distancia).replace('**imagen**',imagenPlaneta)
+		
+		  cards.innerHTML+= nuevaPlantilla;
+	});
+	
+});
--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.get ('/',function(req,res){
 
 app.use('/data',express.static(path.join(__dirname,'/data')));
 app.use('/static',express.static(path.join(__dirname,'assets')));
+app.use('/static',express.static(path.join(__dirname,'node_modules')));
+
 
 app.listen(8080);
